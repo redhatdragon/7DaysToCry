8a1d1e2f4aefe65e9fa0f2c2caee777c10eae722
redhatdragon <paulabrams99@yahoo.com>
2022-11-26 10:15:30 -0500

    Prototyped mesh rendering

--- main.cpp
+++ main.cpp
@@ -1,18 +1,60 @@
 #pragma once
 
-#include <IO_API/IO_API.h>
+#include <IO_API/IO_API_3D.h>
 #include <iostream>
 
 
 
-void appStart() {
+void* tex;
+void* mesh;
+void IO_appStart() {
 	// Ran at the start of our app...
+	std::string path = IO_getDirData();
+	path += "Textures/Bullet.png";
+	//tex = IO_getTexture(path.c_str());
+	path = IO_getDirData();
+	path += "Meshes/Shield.obj";
+	mesh = IO_getMesh(path.c_str());
 }
 
-void appLoop() {
+void IO_appLoop() {
 	// Ran every render frame of our app...
+	/*IO_Point2Df points[] =
+	{
+		-0.5f, -0.5f,
+		0.5f, -0.5f,
+		0.0f, 0.5f
+	};*/
+	/*IO_Point2Df points[] =
+	{
+		-0.5f*50, -0.5f*50,
+		0.5f, -0.5f*50,
+		0.0f*50, 0.5f*50
+	};*/
+	/*IO_Point2Df points[] =
+	{
+		0.0f, 1.0f,
+		1.0f, 1.0f,
+		0.5f, 0.0f
+	};
+	for (uint32_t i = 0; i < 3; i++) {
+		points[i].x *= 50;
+		points[i].y *= 50;
+		points[i].x += 50;
+		points[i].y += 50;
+	}
+	IO_ColorR8G8B8 colors[] =
+	{
+		{255, 0, 0},
+		{0, 255, 0},
+		{0, 0, 255}
+	};
+	IO_drawTriangle2DRGBVertex(points, colors);*/
+	//IO_drawRect(5, 5, 64, 64, 255, 125, 65, 255);
+	//IO_drawTexture(tex, 5, 5, 64, 64);
+	IO_drawMesh(mesh);
 }
 
-void appEnd() {
+void IO_appEnd() {
 	// Ran before quitting our app...
 }
